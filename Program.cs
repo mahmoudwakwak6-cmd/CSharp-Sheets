a75719a55d943e99dfc8a2e74ab9932c7ff7287b
mahmoud-rageh-cmd <mahmoudwakwak6@gmail.com>
2026-07-03 10:27:06 +0300

    week 1 TASK

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// type casting program:-
+// string str = "200";
+// int res = 0;
+// bool istrue = int.TryParse(str, out res);
+// Console.WriteLine(res);
+
+
+using System;
+
+class Program
+{
+    static void Main ()
+    {
+
+        Console.WriteLine("Enter the dimensions of the shape in which you want to place the dominoes.");
+        int x = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+        int res = (x * y) / 2;
+        Console.WriteLine("The maximum number of dominoes you can place is = " + res);
+    }
+};
+
+
